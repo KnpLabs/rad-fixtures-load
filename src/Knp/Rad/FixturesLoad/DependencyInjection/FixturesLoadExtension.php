a0c5621555e8d1d9d0f00db20c78b0860a155f70
Sergio Gómez <sergio@uco.es>
2016-01-12 12:40:57 +0100

    Change services loading from YAML to XML

--- src/Knp/Rad/FixturesLoad/DependencyInjection/FixturesLoadExtension.php
+++ src/Knp/Rad/FixturesLoad/DependencyInjection/FixturesLoadExtension.php
@@ -5,7 +5,7 @@ namespace Knp\Rad\FixturesLoad\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class FixturesLoadExtension extends Extension
 {
@@ -14,7 +14,7 @@ class FixturesLoadExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
